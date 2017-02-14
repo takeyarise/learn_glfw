b02e40e983d77eab43f95b92374724ef9a2641dd
siitake1010 <take1010ct@gmail.com>
2017-02-14 21:25:55 +0900

    change main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <GLFW/glfw3.h>
 
 int main()
@@ -6,17 +8,21 @@ int main()
 
 	if (!glfwInit())
 	{
+		std::cerr << "can't initialize glfw" << std::endl;
 		return -1;
 	}
 
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
 	{
+		std::cerr << "can't create window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
+
+	glClearColor(0.2, 0.2, 0.2, 1.0);
 
 	while (!glfwWindowShouldClose(window))
 	{
